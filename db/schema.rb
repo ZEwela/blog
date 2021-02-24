33fd56f18f0d947121ebf066f6f2c60dc78ab2cd
ZEwela <ewelinazawol@gmail.com>
2021-02-24 19:33:21 +0100

    Category Model Added Associantions Created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_21_231427) do
+ActiveRecord::Schema.define(version: 2021_02_24_181812) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -20,6 +20,21 @@ ActiveRecord::Schema.define(version: 2021_02_21_231427) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "excerpt"
     t.string "location"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_articles_on_user_id"
+  end
+
+  create_table "articles_categories", id: false, force: :cascade do |t|
+    t.integer "article_id", null: false
+    t.integer "category_id", null: false
+    t.index ["article_id", "category_id"], name: "index_articles_categories_on_article_id_and_category_id"
+    t.index ["category_id", "article_id"], name: "index_articles_categories_on_category_id_and_article_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "profiles", force: :cascade do |t|
@@ -41,5 +56,6 @@ ActiveRecord::Schema.define(version: 2021_02_21_231427) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "articles", "users"
   add_foreign_key "profiles", "users"
 end
