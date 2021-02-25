e7e8f97ae3cad6f9fc69beb6dd5c385998f73238
ZEwela <ewelinazawol@gmail.com>
2021-02-25 21:43:58 +0100

    Validation Added to User, Article And Comment

--- app/models/article.rb
+++ app/models/article.rb
@@ -8,8 +8,13 @@ class Article < ApplicationRecord
   scope :published, -> { where.not(published_at: nil) }
   scope :draft, -> { where(published_at: nil) }
   scope :recent, -> { where('articles.published_at > ?', 1.week.ago.to_date) }
+  scope :where_title, -> (term) {where("articles.title LIKE ?", "%#{term}%") }
 
   def long_title
     "#{title} - #{published_at}"
+  end
+
+  def published?
+    published_at.present?
   end
 end
