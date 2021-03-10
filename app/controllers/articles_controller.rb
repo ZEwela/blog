a42114d1e4aa8867c67e2b8f1e6eca577ca9b34f
ZEwela <ewelinazawol@gmail.com>
2021-03-10 20:31:21 +0100

    Controller and Views Updated to Use EmailAFriend Model

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,6 +9,7 @@ class ArticlesController < ApplicationController
 
   # GET /articles/1 or /articles/1.json
   def show
+    @email_a_friend = EmailAFriend.new
   end
 
   # GET /articles/new
@@ -61,8 +62,14 @@ class ArticlesController < ApplicationController
   end
 
   def notify_friend
-    NotifierMailer.email_friend(@article, params[:name], params[:email]).deliver_later 
-    redirect_to @article, notice: 'Successfully sent a message to your friend'
+    @email_a_friend = EmailAFriend.new(email_a_friend_params)
+
+    if @email_a_friend.valid?
+      NotifierMailer.email_friend(@article, @email_a_friend.name, @email_a_friend.email).deliver_later
+      redirect_to @article, notice: 'Successfully sent a message to your friend'
+    else
+      render :notify_friend, status: :unprocessable_entity
+    end
   end
 
   private
@@ -75,4 +82,9 @@ class ArticlesController < ApplicationController
     def article_params
       params.require(:article).permit(:title, :cover_image, :remove_cover_image, :location, :excerpt, :body, :published_at, category_ids: [])
     end
+
+    def email_a_friend_params
+      params.require(:email_a_friend).permit(:name, :email)
+    end
+    
 end
